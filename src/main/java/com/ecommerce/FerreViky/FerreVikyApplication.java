10c625a603e4e1fd509f1d4066808e03ad15ed5c
Nathan <nathanvargasjosue@gmail.com>
2026-06-06 19:40:05 -0600

    Initial commit

---
+++ src/main/java/com/ecommerce/FerreViky/FerreVikyApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.FerreViky;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FerreVikyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FerreVikyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecommerce/FerreViky/FerreVikyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.FerreViky;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FerreVikyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
