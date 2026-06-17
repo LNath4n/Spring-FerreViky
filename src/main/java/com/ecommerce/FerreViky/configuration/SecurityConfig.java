66b5fc92842831e168a2235b60a7c0a654f60546
Nathan <nathanvargasjosue@gmail.com>
2026-06-16 21:26:32 -0600

    Pruebas de Cors

--- src/main/java/com/ecommerce/FerreViky/configuration/SecurityConfig.java
+++ src/main/java/com/ecommerce/FerreViky/configuration/SecurityConfig.java
@@ -70,10 +70,12 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("*"));
+        config.setAllowedOrigins(List.of(
+                "http://localhost:4200"
+        ));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
-        //config.setAllowCredentials(true);
+        config.setAllowCredentials(true);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", config);
